cae00b2a31ea366e8df0fe328670066a8456667f
Victor Rentea <victorrentea@gmail.com>
2017-06-08 14:31:33 +0300

    Manual AOP + measure ex time

--- src/main/java/victor/clean/lambdas/higher/WrapAround.java
+++ src/main/java/victor/clean/lambdas/higher/WrapAround.java
@@ -2,10 +2,28 @@ package victor.clean.lambdas.higher;
 
 public class WrapAround {
 	public static void main(String[] args) {
-		insertLanguages();
-		insertCountries();
+		executeInTx(WrapAround::insertLanguages);
+		executeInTx(WrapAround::insertCountries);
 	}
 	
+	public static void countTime(Runnable r) {
+		long t0 = System.nanoTime();
+		r.run();
+		long t1 = System.nanoTime();
+		System.out.println("A durat " + (t1-t0));
+	}
+	
+	public static void executeInTx(Runnable r) {
+		try {
+			System.out.println("Start Tx");
+			r.run();
+			System.out.println("Commit Tx");
+		} catch (RuntimeException e) {
+			System.out.println("Rollback Tx");
+		} finally {
+			System.out.println("Release Tx res");
+		}
+	}
 	
 	// System.nanoTime();
 
