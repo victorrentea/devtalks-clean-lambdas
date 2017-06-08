905e8a315080eb28ddc313a2a2184fc2088ace95
Victor Rentea <victorrentea@gmail.com>
2017-06-08 14:26:41 +0300

    creepy way of composing functions

--- src/main/java/victor/clean/lambdas/higher/Compose.java
+++ src/main/java/victor/clean/lambdas/higher/Compose.java
@@ -1,10 +1,16 @@
 package victor.clean.lambdas.higher;
 
+import java.util.function.Function;
+
 public class Compose {
 	
 	public static void main(String[] args) {
 		String s = "a <    B";
-		s = collapseSpaces(escapeHtml(s.toUpperCase())); // that's a lot simpler ! just do f(g(h(x)))
+		Function<String, String> normalize = String::toUpperCase;
+		normalize = normalize.andThen(Compose::escapeHtml);
+		normalize = normalize.andThen(Compose::collapseSpaces);
+		s = normalize.apply(s);
+//		s = collapseSpaces(escapeHtml(s.toUpperCase())); // that's a lot simpler ! just do f(g(h(x)))
 		System.out.println(s);
 	}
 
