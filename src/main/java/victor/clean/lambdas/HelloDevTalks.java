6b45b64bb95a31d74110752560cfb49bf7867902
Victor Rentea <victorrentea@gmail.com>
2017-06-08 13:58:52 +0300

    finding items

--- src/main/java/victor/clean/lambdas/HelloDevTalks.java
+++ src/main/java/victor/clean/lambdas/HelloDevTalks.java
@@ -1,5 +1,34 @@
 package victor.clean.lambdas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelloDevTalks {
 
+	public static void main(String[] args) {
+		List<Order> orders = new ArrayList<>();
+		
+		 boolean found = orders.stream()
+			 .anyMatch(o -> o.isNotDelivered());
+		
+//		 boolean found = orders.stream()
+//			 .filter(o -> o.isNotDelivered())
+//			 .findAny()
+//			 .isPresent();
+//		
+//		 boolean found = orders.stream()
+//			 .filter(o -> o.isNotDelivered())
+//			 .count() >= 1;
+//		
+//		 boolean found = !orders.stream()
+//			 .filter(o -> o.isNotDelivered())
+//			 .collect(toList())
+//			 .isEmpty();
+//		
+		// boolean found = orders.stream()
+		// .filter(o -> o.isNotDelivered())
+		// .collect(toList())
+		// .size() >= 1;
+		//
+	}
 }
