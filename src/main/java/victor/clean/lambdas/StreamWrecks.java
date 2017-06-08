19143d827ea47196c0b7784dd7983c1b4ee1a539
Victor Rentea <victorrentea@gmail.com>
2017-06-08 14:08:17 +0300

    convert

--- src/main/java/victor/clean/lambdas/StreamWrecks.java
+++ src/main/java/victor/clean/lambdas/StreamWrecks.java
@@ -8,16 +8,20 @@ import static java.util.stream.Collectors.toSet;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class StreamWrecks {
 	
-	public Set<Product> stuff(List<Order> orders) {
-		return orders.stream()
+	public Set<Product> getMissingProducts(List<Order> orders) {
+		 List<OrderLine> orderLines = orders.stream()
 			.filter(Order::isNotDelivered)
-			.map(Order::getOrderLines)
-			.flatMap(List::stream)
+			.flatMap(order -> order.getOrderLines().stream())
+			.collect(toList());
+		
+		return orderLines.stream()
 			.filter(line -> !line.isInStock())
 			.map(OrderLine::getProduct)
 			.filter(Product::isNotHidden)
@@ -26,16 +30,22 @@ public class StreamWrecks {
 	
 	public List<Product> stuff2(List<Order> orders) {
 		List<Long> excludedProductIds = getExcludedProductIds();
+		
+		Stream<Product> frequentProducts = countProducts(orders)
+				.entrySet().stream()
+				.filter(e -> e.getValue() >= 10)
+				.map(Entry::getKey);
+		
+		return frequentProducts
+				.filter(p->excludedProductIds.contains(p.getId()))
+				.collect(toList());
+	}
+
+	private Map<Product, Integer> countProducts(List<Order> orders) {
 		return orders.stream()
 				.filter(o -> o.getCreationDate().isAfter(LocalDate.now().minusYears(1)))
 				.flatMap(o -> o.getOrderLines().stream())
-				.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)))
-				.entrySet()
-				.stream()
-				.filter(e -> e.getValue() >= 10)
-				.map(Entry::getKey)
-				.filter(p->excludedProductIds.contains(p.getId()))
-				.collect(toList());
+				.collect(groupingBy(OrderLine::getProduct, summingInt(OrderLine::getItemCount)));
 	}
 
 	private List<Long> getExcludedProductIds() {
