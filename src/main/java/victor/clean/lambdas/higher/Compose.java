ae1f30c8050948e5d9f94e2636c0e6c02b640100
Victor Rentea <victorrentea@gmail.com>
2017-06-08 14:31:37 +0300

    compose

--- src/main/java/victor/clean/lambdas/higher/Compose.java
+++ src/main/java/victor/clean/lambdas/higher/Compose.java
@@ -1,16 +1,14 @@
 package victor.clean.lambdas.higher;
 
-import java.util.function.Function;
-
 public class Compose {
 	
 	public static void main(String[] args) {
 		String s = "a <    B";
-		Function<String, String> normalize = String::toUpperCase;
-		normalize = normalize.andThen(Compose::escapeHtml);
-		normalize = normalize.andThen(Compose::collapseSpaces);
-		s = normalize.apply(s);
-//		s = collapseSpaces(escapeHtml(s.toUpperCase())); // that's a lot simpler ! just do f(g(h(x)))
+//		Function<String, String> normalize = String::toUpperCase;
+//		normalize = normalize.andThen(Compose::escapeHtml);
+//		normalize = normalize.andThen(Compose::collapseSpaces);
+//		s = normalize.apply(s);
+		s = collapseSpaces(escapeHtml(s.toUpperCase())); // that's a lot simpler ! just do f(g(h(x)))
 		System.out.println(s);
 	}
 
